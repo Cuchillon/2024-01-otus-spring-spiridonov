d0c2f53f2c6c1c3e65266212d82e4b5c2983d1f1
Cuchillon <ferickadmin@gmail.com>
2024-03-10 00:36:30 +0300

    Hw05 Adding methods into book repository

--- hw05/src/main/kotlin/com/ferick/model/Book.kt
+++ hw05/src/main/kotlin/com/ferick/model/Book.kt
@@ -1,7 +1,7 @@
 package com.ferick.model
 
 data class Book(
-    val id: Long,
+    var id: Long,
     val title: String,
     val author: Author,
     val genres: MutableList<Genre> = mutableListOf()
--- hw05/src/main/kotlin/com/ferick/repositories/impl/JdbcBookRepository.kt
+++ hw05/src/main/kotlin/com/ferick/repositories/impl/JdbcBookRepository.kt
@@ -1,5 +1,6 @@
 package com.ferick.repositories.impl
 
+import com.ferick.exceptions.EntityNotFoundException
 import com.ferick.model.Author
 import com.ferick.model.Book
 import com.ferick.model.Genre
@@ -7,7 +8,9 @@ import com.ferick.repositories.BookRepository
 import com.ferick.repositories.GenreRepository
 import org.springframework.jdbc.core.ResultSetExtractor
 import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
+import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.stereotype.Repository
 import java.sql.ResultSet
 
@@ -26,7 +29,14 @@ class JdbcBookRepository(
     }
 
     override fun findById(id: Long): Book? {
-        TODO("Not yet implemented")
+        val sql = "SELECT b.id, b.title, b.author_id, a.full_name AS author_full_name " +
+                "FROM books AS b " +
+                "JOIN authors AS a " +
+                "ON b.author_id = a.id " +
+                "WHERE id = :id"
+        val params = MapSqlParameterSource()
+            .addValue("id", id)
+        return jdbc.query(sql, params, BookResultSetExtractor())
     }
 
     override fun save(book: Book): Book {
@@ -38,13 +48,16 @@ class JdbcBookRepository(
     }
 
     override fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+        val params = MapSqlParameterSource()
+            .addValue("id", id)
+        jdbc.update("DELETE FROM books WHERE id = :id", params)
     }
 
     private fun getAllBooksWithoutGenres(): List<Book> {
         val sql = "SELECT b.id, b.title, b.author_id, a.full_name AS author_full_name " +
                 "FROM books AS b " +
-                "JOIN authors AS a ON b.author_id = a.id"
+                "JOIN authors AS a " +
+                "ON b.author_id = a.id"
         return jdbc.query(sql, BookRowMapper())
     }
 
@@ -70,11 +83,54 @@ class JdbcBookRepository(
     }
 
     private fun insert(book: Book): Book {
-        TODO("Not yet implemented")
+        val keyHolder = GeneratedKeyHolder()
+        val params = MapSqlParameterSource()
+            .addValue("title", book.title)
+            .addValue("author_id", book.author.id)
+        jdbc.update(
+            "INSERT INTO books (title, author_id) VALUES (:title, :author_id)",
+            params,
+            keyHolder,
+            arrayOf("id")
+        )
+        book.id = keyHolder.getKeyAs(Long::class.java)!!
+        batchInsertGenresRelationsFor(book)
+        return book
     }
 
     private fun update(book: Book): Book {
-        TODO("Not yet implemented")
+        val params = MapSqlParameterSource()
+            .addValue("id", book.id)
+            .addValue("title", book.title)
+            .addValue("author_id", book.author.id)
+        val affectedRows = jdbc.update(
+            "UPDATE books SET title = :title, author_id = :author_id WHERE id = :id",
+            params
+        )
+        if (affectedRows == 0) {
+            throw EntityNotFoundException("Book with id ${book.id} not found")
+        }
+        removeGenresRelationsFor(book)
+        batchInsertGenresRelationsFor(book)
+        return book
+    }
+
+    private fun batchInsertGenresRelationsFor(book: Book) {
+        val parameterSources = book.genres.map { genre ->
+            MapSqlParameterSource()
+                .addValue("book_id", book.id)
+                .addValue("genre_id", genre.id)
+        }.toTypedArray()
+        jdbc.batchUpdate(
+            "INSERT INTO books_genres (book_id, genre_id) VALUES (:book_id, :genre_id)",
+            parameterSources
+        )
+    }
+
+    private fun removeGenresRelationsFor(book: Book) {
+        val params = MapSqlParameterSource()
+            .addValue("book_id", book.id)
+        jdbc.update("DELETE FROM books_genres WHERE book_id = :book_id", params)
     }
 
     private class BookRowMapper : RowMapper<Book> {
